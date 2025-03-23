6fab94461f854adf60c3e17a32c2ba774629b896
William Angulo <williamquerry2@gmail.com>
2025-03-23 16:30:56 -0500

    Se agregaron: -datos.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+from datos import productos
 
+# Creamos el objeto de la clase Flask
 app = Flask(__name__)
 
+# Definimos las rutas
 @app.route("/")
+def index():
+    return render_template("index.html", productos=productos)
 
-def hola():
-    return render_template("index.html")
+@app.route("/producto/<string:nombre>")
+def producto(nombre):
+    producto = [p for p in productos if p['nombre'] == nombre]
+    return render_template("producto.html", producto=producto[0])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
